7c1063b085216a9c97b2b05bb58173b49712720c
Christos Pontikis <pontikis@gmail.com>
2012-07-12 18:52:44 +0300

    datetime functions date_string_format(), now(), date_decode()

--- app/common/utils.php
+++ app/common/utils.php
@@ -1,6 +1,42 @@
 <?php
 
 /**
+ * @param $date_string
+ * @param $format_string
+ * @param $timezone_string
+ * @return string
+ */
+function date_string_format($date_string, $format_string, $timezone_string) {
+    $tz = new DateTimeZone($timezone_string);
+    $date = new DateTime($date_string);
+    $date->setTimeZone($tz);
+    return $date->format($format_string);
+}
+
+/**
+ * @param string $format_string
+ * @param string $str_timezone
+ * @return string
+ */
+function now($format_string = PREF_DATE_FORMAT_TIMESTAMP_FULL, $str_timezone = CONST_DEFAULT_TIMEZONE) {
+    return date_string_format('', $format_string, $str_timezone);
+}
+
+/**
+ * Converts a 14-digit timestamp to date string
+ * @param $ts
+ * @param string $format
+ * @param string $str_timezone
+ * @return string
+ */
+function date_decode($ts, $format = PREF_DATE_FORMAT_DATETIME, $str_timezone = CONST_DEFAULT_TIMEZONE) {
+    $tz = new DateTimeZone($str_timezone);
+    $date = new DateTime($ts);
+    $date->setTimeZone($tz);
+    return $date->format($format);
+}
+
+/**
  * Timezones list with GMT offset
  * http://stackoverflow.com/a/9328760
  * @return array
@@ -24,7 +60,7 @@ function tz_list() {
  */
 function escapeJsonString($value) { # list from www.json.org: (\b backspace, \f formfeed)
 
-    if(function_exists('json_encode')) {
+    if (function_exists('json_encode')) {
         return json_encode($value);
     } else {
         $escapers = array("\\", "/", "\"", "\n", "\r", "\t", "\x08", "\x0c");
