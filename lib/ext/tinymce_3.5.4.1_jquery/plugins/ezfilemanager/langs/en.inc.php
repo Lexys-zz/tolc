ab2e07ae5f338a151f07c8a5a339f46458ad0b30
Christos Pontikis <pontikis@gmail.com>
2012-07-13 15:13:31 +0300

    LIB tinymce_3.5.4.1_jquery, ezfilemanager v2.5

---
+++ lib/ext/tinymce_3.5.4.1_jquery/plugins/ezfilemanager/langs/en.inc.php
@@ -0,0 +1,82 @@
+<?php
+	/**
+	 * $Id: english  014 08-01-2010 Naz $
+	 * Prefix: en
+	 * All langs should be UTF-8
+	*/
+define("TXT_EZFM","ezFilemanager");
+define("TXT_EZ_URL","WWW.WEBNAZ.NET");
+define("TXT_HOME","Home");
+define("TXT_DELETE","Delete Selected");
+define("TXT_SELECT_ALL","Select all");
+define("TXT_QUSTION","Do you really want to Delete Selected  Files/Directories?");
+define("TXT_DO_CLICK","If you are sure click 'OK', otherwise click 'Cancel'");
+define("TXT_FILE_NO","No File selected");
+define("TXT_NON_WRITABLE","THE DIRECTORY IS NOT WRITABLE");
+define("TXT_ERROR","Error");
+define("TXT_HACK","Illigal Characters in path. <a href='./'>".TXT_HOME."</a>");
+define("TXT_NOT_ALLOWED","Access Denied - You are not allowed to view this page. <a href='./'>".TXT_HOME."</a>");
+define("TXT_YOU_CAN","Allowed file extensions");
+define("TXT_MAX_UPLOAD","Max. File size:");
+define("TXT_INSERT","Insert");
+define("TXT_WAITING","Waiting for update...");
+define("TXT_NEW_DIR","New Directory");
+define("TXT_MAKE_DIR_IN","Create Directory in");
+define("TXT_MAKE","Create");
+define("TXT_CREATING","Creating Directory");
+define("TXT_DIR_NO","Directory name is empty");
+define("TXT_DIR_EXIST","Directory allready Exists");
+define("TXT_DIR_CREATED","Directory Created");
+//UPLOAD
+define("TXT_SELECT_FILE","Select File");
+define("TXT_UPLOAD","Upload");
+define("TXT_UPLOAD_ADD","Add Upload");
+define("TXT_UPLOAD_TO","Upload to");
+define("TXT_RENAME","Upload as (rename)");
+define("TXT_UPLOADING","Uploading");
+//
+define("TXT_FILE_EXIST","File allready Exists");
+define("TXT_FILE_BIG","File too big");
+define("TXT_FILE_INVALID","Invalid file");
+//
+define("TXT_CLICK_EDIT","Double Click to rename....");
+define("TXT_ALL","All Files");
+define("TXT_MEDIA","Media Files");
+define("TXT_IMAGE","Image Files");
+define("TXT_FILE","Other Files");
+define("TXT_FILTER_BY","View File Types");
+define("TXT_TYPE_ERROR","Wrong File Types");
+define("TXT_WRONG_KEY","WRONG KEY");
+define("TXT_PATH_ERROR","Wrong Path");
+define("TXT_REMOVE","Remove");
+define("TXT_NEW_DIR_INFO","(Each directory in new line)");
+define("TXT_OPEN_FOLDER","Click to browse");
+define("TXT_CLICK_ICON","Click for more info");
+define("TXT_CLICK","Click on any file icon...");
+define("TXT_CLOSE","Close");
+define("TXT_CREATE_THUMB","Create thumb");
+define("TXT_WIDTH","New Width");
+//INFO
+define("TXT_DIRECTORIES","Directories:");
+define("TXT_FILE_TYPES","File Types:");
+define("TXT_FILES","Files:");
+define("TXT_FILE_NAME","File:");
+define("TXT_SIZE","Size:");
+define("TXT_DIMENSION","Dimension:");
+define("TXT_PERMISSION","Permissions:");
+define("TXT_CREATED","Created:");
+define("TXT_MODIFIED","Modified:");
+define("TXT_DOWNLOAD","Download");
+//
+define("TXT_PASTE","Paste");
+define("TXT_COPY","Copy");
+define("TXT_FILETO_COPY","File to Copy");
+define("TXT_DELETE_FILE","Delete");
+define("TXT_CANCEL_COPY","Cancel Copy");
+//LOGIN
+define("TXT_LOGIN","Login");
+define("TXT_USERNAME","Username");
+define("TXT_PASSWORD","Password");
+define("TXT_LOGOUT","Logout");
+define("TXT_FORM_ERROR","Cannot verify form");
+?>
