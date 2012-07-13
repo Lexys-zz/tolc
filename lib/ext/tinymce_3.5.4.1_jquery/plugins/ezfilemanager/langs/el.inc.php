74e63400fff78f3b6c4e6548e14f6f9e69c097b7
Christos Pontikis <pontikis@gmail.com>
2012-07-13 15:35:32 +0300

    LIB ezfilemanager v2.5 el lang files

---
+++ lib/ext/tinymce_3.5.4.1_jquery/plugins/ezfilemanager/langs/el.inc.php
@@ -0,0 +1,82 @@
+<?php
+	/**
+	 * $Id: english  014 08-01-2010 Naz $
+	 * Prefix: en
+	 * All langs should be UTF-8
+	*/
+define("TXT_EZFM","Αρχεία στον διακομιστή");
+define("TXT_EZ_URL","WWW.WEBNAZ.NET");
+define("TXT_HOME","Αρχή");
+define("TXT_DELETE","Διαγραφή επιλεγμένων");
+define("TXT_SELECT_ALL","Επιλογή όλων");
+define("TXT_QUSTION","Να διαγραφούν τα επιλεγμένα Αρχεία/Φάκελλοι;");
+define("TXT_DO_CLICK","Αν είστε σίγουρος/η, πατήστε 'OK', διαφορετικά 'Cancel'");
+define("TXT_FILE_NO","Δεν έχει επιλογεί κάποιο αρχείο");
+define("TXT_NON_WRITABLE","ΔΕΝ ΕΠΙΤΡΕΠΕΤΑΙ Η ΕΓΓΡΑΦΗ ΣΤΟ ΦΑΚΕΛΛΟ");
+define("TXT_ERROR","Λάθος");
+define("TXT_HACK","Ακατάλληλοι χαρακτήρες στη διαδρομή αρχείου. <a href='./'>".TXT_HOME."</a>");
+define("TXT_NOT_ALLOWED","Η πρόσβαση δεν επιτρέπεται - Δεν μπορείτε να δείτε αυτή τη σελίδα. <a href='./'>".TXT_HOME."</a>");
+define("TXT_YOU_CAN","Επιτρεπόμενοι τύποι αρχείων");
+define("TXT_MAX_UPLOAD","Μέγιστο μέγεθος αρχείου:");
+define("TXT_INSERT","Εισαγωγή");
+define("TXT_WAITING","Σε αναμονή για ενημέρωση...");
+define("TXT_NEW_DIR","Νέος φάκελλος");
+define("TXT_MAKE_DIR_IN","Δημιουργία φακέλλου σε");
+define("TXT_MAKE","Δημιουργία");
+define("TXT_CREATING","Δημιουργία φακέλλου");
+define("TXT_DIR_NO","Το όνομα του φακέλλου είναι κενό");
+define("TXT_DIR_EXIST","Ο φάκελλος υπάρχει ήδη");
+define("TXT_DIR_CREATED","Ο φάκελλος δημιουργήθηκε");
+//UPLOAD
+define("TXT_SELECT_FILE","Επιλογή αρχείου");
+define("TXT_UPLOAD","Ανεβάστε");
+define("TXT_UPLOAD_ADD","Νέο αρχείο για ανέβασμα");
+define("TXT_UPLOAD_TO","Ανεβάστε σε");
+define("TXT_RENAME","Ανεβάστε σαν");
+define("TXT_UPLOADING","Γίνεται ανέβασμα");
+//
+define("TXT_FILE_EXIST","Το αρχείο υπάρχει ήδη");
+define("TXT_FILE_BIG","Το αρχείο έχει μέγεθος μεγαλύτερο από το επιτρεπόμενο");
+define("TXT_FILE_INVALID","Ακατάλληλο αρχείο");
+//
+define("TXT_CLICK_EDIT","Διπλό κλίκ για μετονομασία...");
+define("TXT_ALL","Όλα τα αρχεία");
+define("TXT_MEDIA","Αρχεία πολυμέσων");
+define("TXT_IMAGE","Αρχεία εικόνων");
+define("TXT_FILE","Αλλα αρχεία");
+define("TXT_FILTER_BY","Εμφάνιση τύπων αρχείων");
+define("TXT_TYPE_ERROR","Λάθος τύπος αρχείων");
+define("TXT_WRONG_KEY","ΛΑΘΟΣ ΚΛΕΙΔΙ");
+define("TXT_PATH_ERROR","Λάθος διαδρομή");
+define("TXT_REMOVE","Απομάκρυνση");
+define("TXT_NEW_DIR_INFO","(Κάθε φάκελλος σε καινούργια γραμμή)");
+define("TXT_OPEN_FOLDER","Κλίκ για εξερεύνηση");
+define("TXT_CLICK_ICON","Κλίκ για περισσότερες πληροφορίες");
+define("TXT_CLICK","Κλίκ στο εικονίδιο...");
+define("TXT_CLOSE","Κλείσιμο");
+define("TXT_CREATE_THUMB","Μικρογραφία");
+define("TXT_WIDTH","Νέο πλάτος");
+//INFO
+define("TXT_DIRECTORIES","Φάκελλοι:");
+define("TXT_FILE_TYPES","Τύποι αρχείων:");
+define("TXT_FILES","Αρχεία:");
+define("TXT_FILE_NAME","Αρχείο:");
+define("TXT_SIZE","Μέγεθος:");
+define("TXT_DIMENSION","Διάσταση:");
+define("TXT_PERMISSION","Δικαιώματα:");
+define("TXT_CREATED","Δημιουργήθηκε:");
+define("TXT_MODIFIED","Τροποποιηθηκε:");
+define("TXT_DOWNLOAD","Κατέβασμα");
+//
+define("TXT_PASTE","Επικόλληση");
+define("TXT_COPY","Αντιγραφή");
+define("TXT_FILETO_COPY","Αρχείο για αντιγραφή");
+define("TXT_DELETE_FILE","Διαγραφή");
+define("TXT_CANCEL_COPY","Ακύρωση αντιγραφής");
+//LOGIN
+define("TXT_LOGIN","Σύνδεση");
+define("TXT_USERNAME","'Ονομα χρήστη");
+define("TXT_PASSWORD","Κωδικός");
+define("TXT_LOGOUT","Αποσύνδεση");
+define("TXT_FORM_ERROR","Αδύνατη η πιστοποίηση της φόρμας");
+?>
