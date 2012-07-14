aa7492b410bf062a63fe103495661618e60e0ce4
Christos Pontikis <pontikis@gmail.com>
2012-07-14 20:36:27 +0300

    common js functions

---
+++ app/common/utils.js
@@ -0,0 +1,25 @@
+/**
+ * Opens window screen centered.
+ * @param windowWidth the window width in pixels (integer)
+ * @param windowHeight the window height in pixels (integer)
+ * @param windowOuterHeight the window outer height in pixels (integer)
+ * @param url the url to open
+ * @param wname the name of the window
+ * @param features the features except width and height (status, toolbar, location, menubar, directories, resizable, scrollbars)
+ */
+function CenterWindow(windowWidth, windowHeight, windowOuterHeight, url, wname, features) {
+    var centerLeft = parseInt((window.screen.availWidth - windowWidth) / 2);
+    var centerTop = parseInt(((window.screen.availHeight - windowHeight) / 2) - windowOuterHeight);
+
+    var misc_features;
+    if (features) {
+        misc_features = ', ' + features;
+    }
+    else {
+        misc_features = ', status=no, location=no, scrollbars=yes, resizable=yes';
+    }
+    var windowFeatures = 'width=' + windowWidth + ',height=' + windowHeight + ',left=' + centerLeft + ',top=' + centerTop + misc_features;
+    var win = window.open(url, wname, windowFeatures);
+    win.focus();
+    return win;
+}
