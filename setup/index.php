560dba485a56484175060a3c3ab1931bf6e7e600
Christos Pontikis <pontikis@gmail.com>
2012-07-20 15:54:54 +0300

    Re-organize file structure

--- setup/index.php
+++ setup/index.php
@@ -10,15 +10,10 @@ $favicon_dist = PROJECT_DIR . 'favicon_dist.ico';
 $settings_php = PROJECT_DIR . '/app/common/settings.php';
 $settings_php_dist = PROJECT_DIR . '/app/common/settings.dist.php';
 
-$error_handler_php = PROJECT_DIR . '/app/common/error_handler.php';
-$error_handler_php_dist = PROJECT_DIR . '/app/common/error_handler.dist.php';
-
-
 $cfgs = array();
 $cfgs[] = array($htaccess, $htaccess_dist);
 $cfgs[] = array($favicon, $favicon_dist);
 $cfgs[] = array($settings_php, $settings_php_dist);
-$cfgs[] = array($error_handler_php, $error_handler_php_dist);
 
 foreach ($cfgs as $cfg) {
     $dest = $cfg[0];
