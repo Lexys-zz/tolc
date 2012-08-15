b8afae53663310efc5b3d3e7199f4b34600731e3
Christos Pontikis <pontikis@gmail.com>
2012-08-15 13:17:22 +0300

    Content management - tinymce plugins - ref #12

--- app/admin/rte/rte.js
+++ app/admin/rte/rte.js
@@ -13,8 +13,9 @@ $(function () {
         script_url: tinymce_url,
 
         // General options
+        mode : "textareas",
         theme: "advanced",
-        plugins: "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,ezfilemanager",
+        plugins: "lists,advlist,style,table,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,media,searchreplace,contextmenu,paste,directionality,noneditable,visualchars,nonbreaking,xhtmlxtras,template,ezfilemanager",
         language: lang,
         entity_encoding: "raw",
         relative_urls: true, // default
@@ -22,8 +23,8 @@ $(function () {
 
         // Theme options
         theme_advanced_buttons1: "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect,|,forecolor,backcolor",
-        theme_advanced_buttons2: "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview",
-        theme_advanced_buttons3: "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,fullscreen|nonbreaking,pagebreak",
+        theme_advanced_buttons2: "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime",
+        theme_advanced_buttons3: "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,fullscreen|nonbreaking",
         theme_advanced_toolbar_location: "top",
         theme_advanced_toolbar_align: "left",
         theme_advanced_statusbar_location: "bottom",
@@ -61,7 +62,7 @@ $(function () {
             date_published: ''
         },
         success: function (data) {
-            $('#rte').html(data);
+            $("#rte").html(data);
         }
     });
 
@@ -73,8 +74,5 @@ $(function () {
         }
     });
 
-    $("#btn_save").click(function () {
-
-    });
 
 });
