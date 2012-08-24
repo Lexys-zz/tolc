6600742c50e03cf8ad8942f2c1085e1fbd22a82b
Christos Pontikis <pontikis@gmail.com>
2012-08-24 22:00:58 +0300

    Send 'set names utf8' after mysql connection (actually not needed with php 5.4 on Archlinux, but needed in Debian with php 5.3)

--- app/common/utils_db.php
+++ app/common/utils_db.php
@@ -12,13 +12,18 @@ function get_db_conn($driver) {
 		case 'mysql':
 		case 'mysqlt':
 		case 'mysqli':
+			$dsn = $driver . '://' . $tolc_conf['dbuser'] . ':' . rawurlencode($tolc_conf['dbpass']) .
+				'@' . $tolc_conf['dbserver'] . '/' .
+				$tolc_conf['domains_db'][$_SERVER['SERVER_NAME']] . $tolc_conf['dsn_options'];
+			$conn = NewADOConnection($dsn);
+			$conn->execute('SET NAMES UTF8'); // TODO it is required to 5.3.3-7+squeeze14 (but not needed to Arclinux php 5.4.4)
+			return $conn;
 		case 'postgres':
 		case 'firebird':
 			$dsn = $driver . '://' . $tolc_conf['dbuser'] . ':' . rawurlencode($tolc_conf['dbpass']) .
 				'@' . $tolc_conf['dbserver'] . '/' .
 				$tolc_conf['domains_db'][$_SERVER['SERVER_NAME']] . $tolc_conf['dsn_options'];
 			$conn = NewADOConnection($dsn);
-			$conn->execute('SET NAMES UTF8');
 			return $conn;
 			break;
 		case 'sqlite':
@@ -26,13 +31,13 @@ function get_db_conn($driver) {
 			return NewADOConnection($tolc_conf['dsn_custom']);
 			break;
 		case 'access':
-		case 'db2':
+		case 'db2':  // TODO not tested - possible & must be removed for php > 4
 			$db =& ADONewConnection($driver);
 			$db->Connect($tolc_conf['dsn_custom']);
 			return $db;
-		    break;
-		case 'odbc_mssql':
-		    $db =& ADONewConnection($driver);
+			break;
+		case 'odbc_mssql': // TODO not tested - possible & must be removed for php > 4
+			$db =& ADONewConnection($driver);
 			$db->Connect($tolc_conf['dsn_custom'], $tolc_conf['dbuser'], $tolc_conf['dbpass']);
 			return $db;
 			break;
